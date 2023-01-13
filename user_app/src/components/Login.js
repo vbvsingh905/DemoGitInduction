15afc819954caeb623b769789bdd8858e0f92925
anmolpreet <anmolpreet.kaur@bjshomedelivery.com>
2023-01-13 16:14:55 +0530

    Login.js and Login.css created BJS-anmol

---
+++ user_app/src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Login.css';
+
+export const Login = () => {
+  return (
+    <div>
+        <section className='login'>
+            <Form className='register-form' id='register-form'>
+                <h2 className='form-title'>LOG IN</h2>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label className="">Email address</Form.Label>
+                    <Form.Control className="" type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </section>
+    </div>
+  )
+}
