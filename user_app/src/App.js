28ee1a7cd281950e940cf268a4145e1a2d612700
anmolpreet <anmolpreet.kaur@bjshomedelivery.com>
2023-01-16 12:11:58 +0530

    Modified in APP.js , Login.js and Login.css BJS-anmol

--- user_app/src/App.js
+++ user_app/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <><Login></Login></>
+      <Login></Login>
     </div>
   );
 }
--- user_app/src/components/Login.js
+++ user_app/src/components/Login.js
@@ -6,32 +6,28 @@ import './Login.css';
 export const Login = () => {
   return (
     <div>
+        <h1 className='form-title'>LOG IN</h1>
         <section className='login'>
-        <div className='container-log'>
-        <div className='log-content'>
-        <div className='logform'>
-        <h2 className='form-title'>LOG IN</h2>
-            <Form className='register-form' id='register-form'>
+        <form className='register-form' id="register-form">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label className="">Email address</Form.Label>
+                    <Form.Label className='ab'>Email address</Form.Label>
                     <Form.Control className="" type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
+                    <Form.Text className='form-text'>
                     We'll never share your email with anyone else.
-                    </Form.Text>
+                    </Form.Text><p></p>
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
+                    <Form.Label className='ab'>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
+                <p></p>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
+                </Form.Group><p></p>
+                <Button variant="primary" type="submit" size="lg">
                     Submit
                 </Button>
-            </Form>
-            </div></div>
+            </form>
         </section>
     </div>
   )
